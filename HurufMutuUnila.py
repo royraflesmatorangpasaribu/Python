2dd4cde8c3ae5f2e29be0b1fe6b32673e927c6b7
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2023-01-16 23:03:39 +0700

    Update HurufMutuUnila.py

--- HurufMutuUnila.py
+++ HurufMutuUnila.py
@@ -2,3 +2,22 @@ nama = input("Masukkan Nama : ")
 npm = eval(input("Masukkan NPM  : "))
 nilai = eval(input("Masukkan Nilai : "))
 
+def convertToHurufMutu():
+    if nilai <= 100 and nilai >= 76:
+        hurufMutu = "A"
+    elif nilai < 76 and nilai >= 71:
+        hurufMutu = "B+"
+    elif nilai < 71 and nilai >= 66:
+        hurufMutu = "B"
+    elif nilai < 66 and nilai >= 61: 
+        hurufMutu = "C+"
+    elif nilai < 61 and nilai >= 56:
+        hurufMutu = "C"
+    elif nilai < 56 and nilai >= 50:
+        hurufMutu = "D"
+    elif nilai < 50 and nilai >= 0:
+        hurufMutu = "E"
+    else : 
+        hurufMutu = "Huruf Mutu Tidak Tersedia"
+    return hurufMutu
+
