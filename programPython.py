ab0978d3dea32a95b9de05ba6dee55076e898252
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2023-01-19 22:26:02 +0700

    Update programPython.py

--- programPython.py
+++ programPython.py
@@ -4,3 +4,9 @@ for i in range(1,101):
     rataRata = sum / 100
 print("Rata-rata = ", rataRata)
 
+s=0
+for i in range(1, 101):
+  x =(i-rataRata)**2
+  s = s + x
+varians = s/(100-1)
+print("s^2 = ", varians)
