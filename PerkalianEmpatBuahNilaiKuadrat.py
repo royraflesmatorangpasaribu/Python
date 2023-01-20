4992da17294d57fce1b1a16db4467a303b27b185
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2023-01-20 21:30:47 +0700

    Create PerkalianEmpatBuahNilaiKuadrat.py

---
+++ PerkalianEmpatBuahNilaiKuadrat.py
@@ -0,0 +1,3 @@
+Angka1, Angka2, Angka3, Angka4 = eval(input("Input Empat Angka dipidahkan Dengan Koma : "))
+result = (Angka1**2)*(Angka2**2)*(Angka3**2)*(Angka4**2)
+print("Output Perkalian Empat Buah Nilai Kuadrat", result)
