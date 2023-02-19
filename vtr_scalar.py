28c4df41129322456bf99e2780238912b660490f
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2023-02-19 22:22:40 +0700

    Python

---
+++ vtr_scalar.py
@@ -0,0 +1,11 @@
+# deklarasi library dengan fungsi yang ada didalamnya
+import numpy as np
+
+# inisiasi vector dan nilai skalar
+list1 = [1,2,3,4,5]
+vtr1 = np.array(list1)
+scalar_value = 10
+
+# operasi perkalian vector-scalar
+vtr_scalar = vtr1 * scalar_value
+print(vtr_scalar)
